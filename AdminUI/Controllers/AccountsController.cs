7654782472271bd7abde9c2110c5715a91a0f27f
dinhviet1211 <dinhviet1211@gmail.com>
2022-10-10 02:05:31 +0700

    Updated code

---
+++ AdminUI/Controllers/AccountsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminUI.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: AccountsController
+        [Route("/managements/accounts")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AccountsController/Details/5
+        [Route("/managements/accounts/details")]
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountsController/Create
+        [Route("/managements/accounts/create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountsController/Edit/5
+        [Route("/managements/accounts/edit")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountsController/Delete/5
+        [Route("/managements/accounts/delete")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
